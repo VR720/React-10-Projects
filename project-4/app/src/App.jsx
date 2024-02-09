f9933271db58fc4244b186745967109ff7628ce7
Vivek <www.vivekeduchigure@gmail.com>
2024-02-09 12:09:48 +0530

    fetching data from server

--- project-4/app/src/App.jsx
+++ project-4/app/src/App.jsx
@@ -1,6 +1,48 @@
+import { useEffect, useState } from "react";
 import styled from "styled-components";
 
+const BASE_URL = "http://localhost:9000/";
+
 const App = () => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchFoodData = async () => {
+      setLoading(true);
+
+      try {
+        const response = await fetch(BASE_URL);
+        const json = await response.json();
+        setData(json);
+        setLoading(false);
+      } catch (error) {
+        setError("unable to fetch data");
+      }
+    };
+
+    fetchFoodData();
+  }, []);
+
+  console.log(data);
+
+  // const temp = [
+  //   [
+  //     {
+  //       name: "Boilded Egg",
+  //       price: 10,
+  //       text:
+  //         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+  //       image: "/images/egg.png",
+  //       type: "breakfast",
+  //     },
+  //   ],
+  // ];
+
+  if (error) return <div>{error}</div>;
+  if (loading) return <div>loading.....</div>;
+
   return (
     <Container>
       <TopContainer>
